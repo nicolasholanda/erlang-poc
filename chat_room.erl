3aa73c444420ca400e53dff4f95ce4a42ca5fcd4
nicolasholanda <nikolas.nhm14@gmail.com>
2025-12-08 09:24:38 -0300

    Added a very simple chat example

---
+++ chat_room.erl
@@ -0,0 +1,20 @@
+-module(chat_room).
+-export([start/0, room_loop/1]).
+
+start() ->
+    RoomPid = spawn(chat_room, room_loop, [[]]).
+
+
+room_loop(Users) ->
+    receive
+        {join, Username, UserPid} ->
+            NewUsers = [{Username, UserPid} | Users],
+            io:format("~s joined~n", [Username]),
+            room_loop(NewUsers);
+        {leave, UserPid} ->
+            NewUsers = lists:filter(fun({_, Pid}) -> Pid =/= UserPid end, Users),
+            room_loop(NewUsers);
+        {message, FromPid, Msg} ->
+            lists:foreach(fun({_, P}) -> P ! {chat, FromPid, Msg} end, Users),
+            room_loop(Users)
+    end.

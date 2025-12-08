e6191c5e7850565b9d8d9203e2fd765ee68c5954
nicolasholanda <nikolas.nhm14@gmail.com>
2025-12-08 09:39:50 -0300

    Added chat actors

---
+++ chat_user.erl
@@ -0,0 +1,20 @@
+-module(chat_user).
+-export([start/2, user_loop/2]).
+
+start(Username, RoomPid) ->
+    UserPid =spawn(chat_user, user_loop, [Username, RoomPid]),
+    RoomPid ! {join, Username, UserPid},
+    UserPid.
+
+user_loop(Username, RoomPid) ->
+    receive
+        {chat, FromPid, Msg} ->
+            io:format("~s received message from ~s: ~s~n", [Username, FromPid, Msg]),
+            user_loop(Username, RoomPid);
+        {send_message, Msg} ->
+            RoomPid ! {message, Username, Msg},
+            user_loop(Username, RoomPid);
+        leave ->
+            RoomPid ! {leave, self()},
+            io:format("~s has left the chat.~n", [Username])
+    end.
